e252370b4c44adc786755cdcc032b346ba5370fc
Tony Wong <63030915+tonytony2020@users.noreply.github.com>
2022-11-12 19:48:03 +0800

    move test video sample file into testdata/ folder

--- parser_test.go
+++ parser_test.go
@@ -9,7 +9,7 @@ import (
 func TestReadStruct(t *testing.T) {
 	// sample fetch from http://techslides.com/sample-webm-ogg-and-mp4-video-files-for-html5
 	// TODO: use much smaller one instead
-	path := "small.webm"
+	path := "testdata/small.webm"
 	r, err := os.Open(path)
 	if err != nil {
 		t.Fatal("unable to open file", path)
